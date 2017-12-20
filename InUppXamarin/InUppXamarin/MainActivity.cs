152a80494c03f854621f8dc638935c2140eaf1bd
Mir Albertioni <mir.al@live.se>
2017-12-20 13:55:53 +0100

    testar bygg

---
+++ InUppXamarin/InUppXamarin/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Net.Http;
+using System.Linq;
+using Newtonsoft.Json;
+using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Android;
+
+namespace AppInUpp
+{
+    [Activity(Label = "InUppXamarin", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        EditText inputLocation;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            AppCenter.Start("8c94596f-4844-4998-a5e7-615b1e0549bd",
+                   typeof(Analytics), typeof(Crashes));
+
+            base.OnCreate(savedInstanceState);
+
+            
+            textView.Text = "";
+            inputLocation.Text = "";
+
+            readJson.Click += delegate
+            {
+                try
+                {
+                    readJson.Click += async delegate
+                    {
+                        using (var client = new HttpClient())
+                        {
+                            var req = new HttpRequestMessage
+                            {
+                                // send a GET request  
+                                RequestUri = new Uri("https://api.resrobot.se/location.name?key=ead40c96-d5e9-4db5-a6a8-2d75ae9c40ed&input=" + inputLocation.Text + "&format=json")
+                            };
+                            req.Headers.Add("Accept", "application/json");
+                            var response = await client.SendAsync(req);
+                            var data = await response.Content.ReadAsStringAsync();
+
+                            var posts = JsonConvert.DeserializeObject<Rootobject>(data);
+                            var x = posts.StopLocation.ToList();
+                            string myText;
+                            foreach(var text in x)
+                            {
+                                myText = text.id + ", " + text.name + "\n" + textView.Text;
+                                textView.Text = myText;
+                            }
+                            //textView.Text = "First post:\n\n" + x;
+                        }
+                    };
+                }
+                catch (Exception e)
+                {
+
+                }
+
+            };
+        }
+    }
+}
+
---
+++ InUppXamarin/InUppXamarin/Rootobject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace AppInUpp
+{
+    public class Rootobject
+    {
+        public Stoplocation[] StopLocation { get; set; }
+    }
+
+    public class Stoplocation
+    {
+        public string id { get; set; }
+        public string extId { get; set; }
+        public string name { get; set; }
+        public float lon { get; set; }
+        public float lat { get; set; }
+        public int weight { get; set; }
+        public int products { get; set; }
+    }
+}
+
